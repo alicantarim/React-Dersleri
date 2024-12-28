79c5bafc4c1cd83aa9fb190236e918f95a93e615
alicantarim <alicantarim@hotmail.com>
2024-12-28 03:09:17 +0300

    completed useState

--- 4.useState/src/App.jsx
+++ 4.useState/src/App.jsx
@@ -11,11 +11,25 @@ function App() {
     "Canercan",
     "Ahmet",
   ]);
+  const [userInfo, setUserInfo] = useState({
+    username: "alicantarim",
+    password: "11223344",
+  });
+  const [count, setCount] = useState(0);
 
   const handleChange = () => {
     // debugger;  -> Debug yapar. console dan inspect'i açarak Source üzerinden takip edebilirsin.
     setFirstName("Lavinya Güneş");
   };
+
+  const arttir = () => setCount(count + 1);
+
+  const azalt = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  console.log("component render edildi.");
 
   return (
     <>
@@ -29,7 +43,28 @@ function App() {
         </div>
         <br />
         <div>
+          <div>
+            {"username: "}
+            {userInfo.username}
+          </div>
+          <div>
+            {"password: "}
+            {userInfo.password}
+          </div>
+        </div>
+        <br />
+        <div>{count}</div>
+        <br />
+        <div>
           <button onClick={handleChange}>İsmi Değiştir</button>
+        </div>
+        <br />
+        <div>
+          <button onClick={arttir}>Arttır</button>
+        </div>
+        <br />
+        <div>
+          <button onClick={azalt}>Azalt</button>
         </div>
       </div>
     </>
